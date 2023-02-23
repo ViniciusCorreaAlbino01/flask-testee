fe05036e9e86ebc91c745c1fb987aaffc5ab19a3
ViniciusCorreaAlbino01 <100493121+ViniciusCorreaAlbino01@users.noreply.github.com>
2023-02-23 16:19:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -208,8 +208,7 @@ def cadastrar():
 
 #colocar o site no ar
 if __name__ == '__main__':
-    port = int(os.getenv('PORT'),'5000')
-    app.run(debug=True, host="0.0.0.0",port=port)
+    app.run(debug=True, host="0.0.0.0",port=5000)
 
 
 
