447dfb09fbd453e8a0cf502bc9cd9c8a155b89cc
ViniciusCorreaAlbino01 <viniciusc.albino@hotmail.com>
2023-02-24 09:21:16 -0300

    adicionando_form

--- main.py
+++ main.py
@@ -10,41 +10,12 @@ from funcoes_trello import *
 from funcoes_C6 import *
 from funcoes_sql import * 
 from funcoes_firebase import *
+from form import *
 
 app = Flask(__name__)
 WTF_CSRF_ENABLED = False
 app.config['WTF_CSRF_ENABLED'] = False
 
-
-
-
-class ClienteForm(FlaskForm):
-    nome = StringField('Nome',validators=[DataRequired('Preencha com seu nome')])
-    cpf = TelField('CPF',validators=[DataRequired('Preencha com seu cpf'), Length(min=11,max=11)] )
-    telefone = TelField('Telefone',validators=[DataRequired('Preencha com seu telefone'),Length(min=11,max=11)])
-    data_nascimento = DateField('data_nascimento',format='%d/%m/%Y' ,validators=[DataRequired()])
-    nome_pai = StringField('nome_pai',validators=[DataRequired('Preencha com o nome do seu pai')])
-    nome_mae = StringField('nome_mae',validators=[DataRequired('Preencha com o nome da sua mãe')])
-    rg = DecimalField('rg',validators=[DataRequired('Preencha com o número do seu rg')])
-    estado_expedidor = SelectField('Estado_expedidor', choices=[('','Selecione...'),('AC','AC'),('AL','AL'),('AP','AP'),('AM','AM'),        
-    ('BA','BA'),('CE','CE'),('DF','DF'),('ES','ES'),('GO','GO'),('MA','MA'),('MT','MT'),('MS','MS'),('MG','MG'),('PA','PA'),('PB','PB'), 
-    ('PR','PR'),('PE','PE'),('PI','PI'),('RJ','RJ'),('RN','RN'),('RS','RS'),('RO','RO'),('RR','RR'),('SC','SC'),('SP','SP'),('SE','SE'), 
-     ('TO','TO')],validators=[DataRequired()])
-    orgao_emissor = SelectField('orgao_emissor', choices=[('','Selecione...'),('SSP','SSP'),('DETRAN','DETRAN'),('CTPS','CTPS'), 
-     ('Outros','Outros')],validators=[DataRequired()])
-    estado = SelectField('Estado', choices=[('','Selecione...'),('AC','AC'),('AL','AL'),('AP','AP'),('AM','AM'),('BA','BA'),('CE','CE'), 
-    ('DF','DF'),('ES','ES'),('GO','GO'),('MA','MA'),('MT','MT'),('MS','MS'),('MG','MG'),('PA','PA'),('PB','PB'),('PR','PR'),('PE','PE'), 
-    ('PI','PI'),('RJ','RJ'),('RN','RN'),('RS','RS'),('RO','RO'),('RR','RR'),('SC','SC'),('SP','SP'),('SE','SE'),('TO','TO')],validators=[DataRequired()])
-    cidade = StringField('Cidade',validators=[DataRequired('Preencha com a cidade onde você mora'), ])
-    cep = DecimalField('cep',validators=[DataRequired('Preencha com o seu cep'),Length(min=8,max=8)])
-    endereco = StringField('Logradouro',validators=[DataRequired('Preencha com o seu logradouro'), ])
-    bairro = StringField('bairro',validators=[DataRequired('Preencha com o seu bairro'), ])
-    num = DecimalField('num',validators=[DataRequired('Preencha com o num da sua residência'), ])
-    banco = DecimalField('banco',validators=[DataRequired('Preencha com o codigo do seu banco'), Length(min=3,max=3)])
-    agencia = DecimalField('agencia',validators=[DataRequired('Preencha com o numero da sua agencia'),Length(min=4,max=4) ])
-    conta = DecimalField('conta',validators=[DataRequired('Preencha com o numero da sua conta'), ])
-    data_expedicao = DateField('data_expedicao',format='%d/%m/%Y',validators=[DataRequired()])
-    sexo = SelectField('Sexo', choices=[('','Selecione...'),('Feminino','Feminino'),('Masculino','Masculino')],validators=[DataRequired()])
 
 class Cliente():
     def __init__(self):
@@ -163,7 +134,7 @@ def contratar():
         Cliente_atual.ade,Cliente_atual.link = finalizar_cadastro_facta(Cliente_atual.token_facta,Cliente_atual.codigo_cliente,Cliente_atual.id_simulador)
         url_facta(Cliente_atual.token_facta,Cliente_atual.ade)
 #         except:
-            return render_template('Erro.html')
+            # return render_template('Erro.html')
         if 'rg' in Cliente_atual.codigo_cliente:
             return render_template('pag2.html', form = form, nome=Cliente_atual.nome,rg = '',nome_mae = Cliente_atual.nome_mae,nome_pai = Cliente_atual.nome_pai,orgao_emissor = Cliente_atual.orgao_emissor,data_expedicao = Cliente_atual.data_expedicao, estado_expedidor = Cliente_atual.estado_expedidor, sexo = Cliente_atual.sexo,cep = Cliente_atual.cep,endereco=Cliente_atual.endereco,bairro=Cliente_atual.bairro,cidade=Cliente_atual.cidade,estado=Cliente_atual.estado,num = Cliente_atual.num)
         if 'data_expedicao' in Cliente_atual.codigo_cliente:
